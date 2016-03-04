6c40ca03aa53e10133128a5352e3040ea19ef514
Konstantin <zamaraevk@gmail.com>
2016-03-04 10:33:43 -0500

    add my email

--- assets/js/formspree.js
+++ assets/js/formspree.js
@@ -2,7 +2,7 @@ var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '//formspree.io/your@email.com',
+		url: '//formspree.io/zamaraevk@gmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
